0e37118500c28b4b79604f2f630254b2c5b0cb89
Seunghoya <viva.primavera121@gmail.com>
2021-12-03 17:45:15 +0900

    위장 update

--- Programmers/Level 2/위장/solution.js
+++ Programmers/Level 2/위장/solution.js
@@ -13,4 +13,17 @@ function solution(clothes) {
     return answer - 1;
 }
 
-// 
+// (a + 1) * (b + 1) * (c + 1) ... 형태를 만들어 주면 됨
+// 마지막에 - 1 해주는건 옷을 안입었을 경우
+// 문제 보고 쉽게 풀 줄 알았는데 객체로 접근하는걸 생각하기까지 하루가 넘게 걸렸다.
+// 근데 reduce로 푼 사람은 뭐야
+
+// 베스트 답
+function solution(clothes) {
+    return Object.values(clothes.reduce((obj, t)=> {
+        obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
+        return obj;
+    } , {})).reduce((a,b)=> a*(b+1), 1)-1;    
+}
+
+// 나도 고인물이고 싶다...
